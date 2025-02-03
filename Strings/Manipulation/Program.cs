3edf03441184369cd219eda203e759127702fe42
thereal7xs <israel6860@bazeuniversity.edu.ng>
2025-02-03 09:48:24 +0100

    Manipulation

--- Strings/Manipulation/Program.cs
+++ Strings/Manipulation/Program.cs
@@ -1,37 +1,52 @@
 ﻿// Application Programming .NET Programming with C# by Abdullahi Tijjani
 // Example file for manipulating string content
 
+using System.Runtime.CompilerServices;
+
 string str1 = "The quick brown fox jumps over the lazy dog.";
 string str2 = "This is a string";
 string str3 = "THIS is a STRING";
 string[] strs = {"one", "two", "three", "four"};
 
 // TODO: Length of a string 
-
+Console.WriteLine(str1.Length);
 
 // TODO: Access individual characters
-
+Console.WriteLine(str1[14]);
 
 // TODO: Iterate over a string like any other sequence of values
-
+foreach (char ch in str1) {
+    Console.Write(ch);
+    if (ch == 'b') {
+        Console.WriteLine();
+        break;
+    }
+}
 
 // TODO: String Concatenation         
 string outstr;
-
+outstr = String.Concat(strs);
 
 // TODO: Joining strings together with Join
-
+outstr = String.Join('.', strs);
+Console.WriteLine(outstr);
+outstr = String.Join("---", strs);
+Console.WriteLine(outstr);
 
 // TODO: String Comparison
 
 // Equals just returns a regular Boolean
-// bool isEqual = str2.Equals(str3);
-// Console.WriteLine($"{isEqual}");
+bool isEqual = str2.Equals(str3);
+Console.WriteLine($"{isEqual}");
 
 // Compare will perform an ordinal comparison and return:
 // < 0 : first string comes before second in sort order
 // 0 : first and second strings are same position in sort order
 // > 0 : first string comes after the second in sort order
 
+int result = String.Compare(str2, "This is a string");
+Console.WriteLine($"{result}");
 
 // TODO: Replacing content
+string outstr1 = str1.Replace("fox", "cat");
+Console.WriteLine($"{outstr1}");
